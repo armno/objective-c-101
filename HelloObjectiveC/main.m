722c4dcb7efb89c3dc15cef02f731da5b951c34d
Armno <monkeyarmno@gmail.com>
2015-06-22 11:37:40 +0700

    Initial Commit

---
+++ HelloObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by armno on 6/22/2558 BE.
+//  Copyright (c) 2558 armno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
