2e061336a4ebe783e6362f9a860bf1c192e29ebb
Armno <monkeyarmno@gmail.com>
2015-06-23 10:18:26 +0700

    add notes about xcode can handle circular imports

--- HelloObjectiveC/Person.h
+++ HelloObjectiveC/Person.h
@@ -8,9 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
-@class Car_old; // "trust me, the Class har exists, so don't try to find it right now"
+@class Car_old;
+// "trust me, the Class exists, so don't try to find it right now"
 // we do this because in Car.h it also imports Person.h
 // it could end up with circular imports
+
+// UPDATE: thanks to @digitalvanilla
+// it seems recent xcode can deal with circular imports now.
+// "the compiler now recorgnize the loop import and act accordingly"
 
 @interface Person : NSObject
 
